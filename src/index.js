6b2b2a1bc477c15070ef9806102e5c9ca76241c1
jmanhart <jmanhart@sentry.io>
2020-01-17 09:48:13 -0600

    Adding in sentry and getting it started

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import * as Sentry from '@sentry/browser';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+
+// Sentry.init({dsn: "https://9b411b5d547a4bb0973431c1fc9cb1b4@sentry.io/1467044"});
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
